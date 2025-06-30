f84e033228f3f585e62cc8d43a9c9ba0cf42b6b9
Set <sattari4@hotmail.com>
2025-06-30 14:19:12 +0200

    cleaned up general preps (#7)

--- src/main/java/com/pet/project/exam/module01/q1DI/commons/ds/EmployeeSalary.java
+++
@@ -1,4 +0,0 @@
-package com.pet.project.exam.module01.q1DI.commons.ds;
-
-public class EmployeeSalary {
-}
--- src/main/java/com/pet/project/exam/module01/q2Pattern/common/renderer/weight/BoldFontWeightRenderer.java
+++
@@ -1,8 +0,0 @@
-package com.pet.project.exam.module01.q2Pattern.common.renderer.weight;
-
-public class BoldFontWeightRenderer implements FontWeightRenderer {
-    @Override
-    public String render(String text) {
-        return "<bold-weight>" + text + "</bold-weight>";
-    }
-}
